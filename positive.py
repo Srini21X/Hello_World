e5cb28b4bc2d1fce4dffdace004f38ff775ec225
Srini21X <42365157+Srini21X@users.noreply.github.com>
2018-08-14 10:10:03 +0530

    guvi 1st project
    find positive number

---
+++ positive.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 14 10:03:45 2018
+
+@author: HP
+"""
+a=int(input())
+if a>0:
+    print('Positive')
+elif a==0:
+    print('Zero')
+else:
+    print('Negative')
